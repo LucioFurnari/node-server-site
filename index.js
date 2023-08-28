7bfeb80e8e7365c9b806d2952b66aafab69b416b
LucioFurnari <luciofurnari99@gmail.com>
2023-08-28 20:21:44 -0300

    Create simple server with node js

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  // Read file
+  fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+    if (err) throw err;
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content, 'utf-8');
+  })
 });
 
 server.listen(5000)
