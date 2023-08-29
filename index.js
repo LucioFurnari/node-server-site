f2fc6f82a3a29b90f54c34509314d04cdb8a32a3
LucioFurnari <luciofurnari99@gmail.com>
2023-08-29 13:33:17 -0300

    Implement the rest of routes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const setPage = require('./functions.js')
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   console.log(req.url)
   switch (req.url) {
     case '/':
-      // Read file
       fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
         if (err) throw err;
         res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -15,20 +15,25 @@ const server = http.createServer((req, res) => {
       })
       break;
     case '/about':
-      // Read file
       fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
       if (err) throw err;
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(content, 'utf-8');
       })
       break;
-    case 'contact-me':
-      fs.readFile(path.join(__dirname, 'public', 'contact-me'), (err, content) => {
+    case '/contact-me':
+      fs.readFile(path.join(__dirname, 'public', 'contact-me.html'), (err, content) => {
         if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf-8');
+      })
+      break;
+    default:
+      fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(content, 'utf-8')
       })
-    default:
       break;
   }
 });
