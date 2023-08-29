c093acfbf522915a6d22990c3393d8cf4e90d65a
LucioFurnari <luciofurnari99@gmail.com>
2023-08-28 22:55:03 -0300

    Create switch function to the route page

--- index.js
+++ index.js
@@ -4,12 +4,33 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  // Read file
-  fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
-    if (err) throw err;
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(content, 'utf-8');
-  })
+  console.log(req.url)
+  switch (req.url) {
+    case '/':
+      // Read file
+      fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf-8');
+      })
+      break;
+    case '/about':
+      // Read file
+      fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+      })
+      break;
+    case 'contact-me':
+      fs.readFile(path.join(__dirname, 'public', 'contact-me'), (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(content, 'utf-8')
+      })
+    default:
+      break;
+  }
 });
 
 server.listen(5000)
