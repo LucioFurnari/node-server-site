b450499b890852feba20fa99d192cb03746d95f1
LucioFurnari <luciofurnari99@gmail.com>
2023-08-30 22:44:05 -0300

    Change again the switch route, and add a variable that save the extension of the file, loading css files

--- index.js
+++ index.js
@@ -6,25 +6,76 @@ const getPage = require('./functions.js')
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   console.log(req.url)
+
+  // Build file path
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+  console.log('Path:' + filePath)
+
+  // File extension
+  let fileExt = path.extname(filePath);
+  console.log('Extension: '+fileExt)
+  // Content type - default
+  let contentType = 'text/html'
+
+  switch (fileExt) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    default:
+      break;
+  }
+
   switch (req.url) {
     case '/':
-      fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+      fs.readFile(filePath, (err, content) => {
         if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content, 'utf-8');
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.write(content, 'utf-8')
+        res.end()
       })
       break;
     case '/about':
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end( getPage('about.html'), 'utf-8');
+      fs.readFile(filePath + '.html', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': contentType});
+        res.write(data, 'utf-8');
+        res.end()
+      });
       break;
     case '/contact-me':
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end( getPage('contact-me.html'), 'utf-8');
+      fs.readFile(filePath + '.html', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': contentType});
+        res.write(data, 'utf-8');
+        res.end()
+      });
+      break;
+    case '/public/css/style.css':
+      fs.readFile(filePath + '.html', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': contentType});
+        res.write(data, 'utf-8');
+        res.end()
+      });
+      break;
+    case '/css/style.css':
+      fs.readFile(filePath, function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': contentType});
+        res.write(data, 'utf-8');
+        res.end()
+      });
       break;
     default:
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end( getPage('404.html'), 'utf-8')
+      fs.readFile(path.join(__dirname, 'public', '404.html'), function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': contentType});
+        res.write(data, 'utf-8');
+        res.end()
+      });
       break;
   }
 });
